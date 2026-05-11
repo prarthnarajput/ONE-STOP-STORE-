60b1a99d5edcd8f944c3350cf1cd2768f1d45122
PRARTHNARAJPUT <prathnadodiya24@gmail.com>
2026-05-11 15:10:04 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,62 @@
+const categorybtns = document.getElementById('category-btns');
+
+let category = ['all',...new Set(products.map(p => p.category))];
+
+function rendercategorybtn(){
+    category.forEach(cat =>{
+        let btn = document.createElement('button');
+        btn.dataset.category=cat;
+        btn.textContent=cat;
+
+        if(cat == 'all')btn.classList.add('active');
+
+        btn.addEventListener('click',(e) =>{
+            document.querySelectorAll('#category-btns button').forEach(btn => btn.classList.remove('active'))
+            e.target.classList.add('active');
+            filtercategory(cat);
+        })
+        categorybtns.appendChild(btn);
+    })
+}
+
+function filtercategory(category){
+    let filtercategory = category == 'all' ? products:products.filter(c => c.category == category);
+    renderproduct(filtercategory)
+}
+
+  
+
+function renderproduct(list = products ){
+const productcontainer = document.getElementById('product-container');
+productcontainer.innerHTML = '';
+   list.forEach(product=>{
+      let productcard = document.createElement('div');
+      productcard.classList.add('productcard');
+
+      productcard.innerHTML = `
+      <img src="${product.image}" alt="${product.name}">
+    <div class="product-info">
+        <h3>${product.name}</h3>
+        <p>${product.description}</p>
+        <p><b>price:</b>${product.price}</p>
+        <div class="bottom">
+            <p><b>category:</b>${product.category}</p>
+            <div class="btn">
+                <button onclick="openproduct(${product.id})">view more</button>
+                <button class="cart">ADD TO CART</button>
+           </div>
+      `;
+
+      productcontainer.appendChild(productcard)
+   }) 
+}
+
+function openproduct(id){
+    window.location.href = `product_detail.html?id=${id}`;
+}
+
+renderproduct();
+rendercategorybtn();
+
+
+  
---
+++ product.js
@@ -0,0 +1,139 @@
+const products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    category: "Electronics",
+    price: 2999,
+    rating: 4.5,
+    stock: 20,
+    description: "Noise-cancelling wireless headphones with deep bass.",
+    image: "02shop.webp",
+    gallery: [
+      "2.1shop.webp",
+      "2.2shop.webp",
+      "2.3shop.webp"
+    ],
+    
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    category: "Electronics",
+    price: 4999,
+    rating: 4.3,
+    stock: 15,
+    description: "Fitness smart watch with heart-rate monitor.",
+    image: "03shop.webp",
+    gallery: [
+      "3.1shop.webp",
+      "3.2shop.webp",
+      "3.3shop.webp"
+    ]
+  },
+  {
+    id: 3,
+    name: "Running Shoes",
+    category: "Fashion",
+    price: 2499,
+    rating: 4.6,
+    stock: 30,
+    description: "Lightweight and comfortable running shoes.",
+    image: "04shop.webp",
+    gallery: [
+      "4.1shop.webp",
+      "4.2shop.webp",
+      "4.3shop.webp"
+    ]
+  },
+  {
+    id: 4,
+    name: "Backpack",
+    category: "Accessories",
+    price: 1799,
+    rating: 4.2,
+    stock: 25,
+    description: "Waterproof backpack for travel and college.",
+    image: "05shop.webp",
+    gallery: [
+      "5.1shop.webp",
+      "5.2shop.webp",
+      "5.3shop.webp"
+    ]
+  },
+  {
+    id: 5,
+    name: "Bluetooth Speaker",
+    category: "Electronics",
+    price: 1999,
+    rating: 4.4,
+    stock: 18,
+    description: "Portable Bluetooth speaker with powerful sound.",
+    image: "06shop.webp",
+    gallery: [
+      "6.1shop.webp",
+      "6.2shop.webp",
+      "6.3shop.webp"
+    ]
+  },
+  {
+    id: 6,
+    name: "Sunglasses",
+    category: "Fashion",
+    price: 999,
+    rating: 4.1,
+    stock: 50,
+    description: "Stylish UV-protected sunglasses.",
+    image: "07shop.webp",
+    gallery: [
+      "7.1shop.webp",
+      "7.2shop.webp",
+      "7.3shop.webp"
+    ]
+  },
+   {
+    id: 7,
+    name: "Power Bank",
+    category: "Accessories",
+    price: 1499,
+    rating: 4.3,
+    stock: 25,
+    description: "10000mAh fast charging power bank",
+    image: "08shop.webp",
+    gallery: [
+      "8.1shop.webp",
+      "8.2shop.webp",
+      "8.3shop.webp"
+    ]
+  },
+   {
+    id: 8,
+    name: "Gaming Mouse",
+    category: "Gaming",
+    price: 999,
+    rating: 4.6,
+    stock: 18,
+    description: "RGB gaming mouse",
+    image: "09shop.webp",
+    gallery: [
+      "9.1shop.webp",
+      "9.2shop.webp",
+      "9.3shop.webp"
+    ]
+  },
+  {
+    id: 9,
+    name: "Web Camera",
+    category: "Electronics",
+    price: 1899,
+    rating: 4.1,
+    stock: 9,
+    description: "HD webcam for online meetings",
+    image: "10shop.webp",
+    gallery: [
+      "10.1shop.webp",
+      "10.2shop.webp",
+      "10.3shop.webp"
+    ]
+  },
+];
+
